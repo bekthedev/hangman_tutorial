5a7b193d08a07911228b4384fb59bfef071bc34d
Bek Johansson <beksaylor@gmail.com>
2025-04-04 16:50:37 -0500

    add getHangmanArt

--- src/main/java/edu/bekthedev/App.java
+++ src/main/java/edu/bekthedev/App.java
@@ -1,13 +1,75 @@
 package edu.bekthedev;
 
-/**
- * Hello world!
- *
- */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class App {
+    public static void main(String[] args) {
+        String word = "watermelon";
+
+        Scanner scanner = new Scanner(System.in);
+        ArrayList<Character> wordState = new ArrayList<>();
+        int wrongGuesses = 0;
+
+        for (int i = 0; i < word.length(); i++) {
+            wordState.add('_');
+        }
+
+        System.out.println("Welcome to Java Hangman!");
+
+        System.out.println(getHangmanArt(1));
+
+        scanner.close();
+    }
+
+    static String getHangmanArt(int wrongGuesses) {
+        return switch (wrongGuesses) {
+            case 0 -> """
+
+
+
+                    """;
+            case 1 -> """
+
+                    O
+
+
+                    """;
+            case 2 -> """
+
+                    O
+                    |
+
+                    """;
+            case 3 -> """
+
+                     O
+                    /|
+
+                     """;
+            case 4 -> """
+
+                     O
+                    /|\
+
+                     """;
+
+            case 5 -> """
+
+                     O
+                    /|\\
+                    /
+                     """;
+
+            case 6 -> """
+
+                     O
+                    /|\\
+                    / \\
+                     """;
+            default -> "";
+
+        };
+
     }
 }
